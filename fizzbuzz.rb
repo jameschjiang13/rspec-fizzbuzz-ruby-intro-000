f7bfadacb24b8c6adbd56b247084553c2704ea22
James Jiang <monochrome473@gmail.com>
2020-03-24 20:08:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
   if int % 15 == 0
-    puts "FizzBuzz"
+    FizzBuzz
   elsif int % 3 == 0
-    puts "Fizz"
+    Fizz
   elsif int % 5 == 0
-    puts "Buzz"
+    Buzz
   else
   end
 end
